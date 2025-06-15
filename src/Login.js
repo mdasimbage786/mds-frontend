f98151b005b1c487fdd5c136157943b82c5a4f59
Mohammedasim Bage <mdasimbage786@gmail.com>
2025-06-15 23:44:03 +0530

    Resolve Login Route

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Login.css';
-import { motion } from 'framer-motion';
 import axios from 'axios';
 
 const Login = () => {
